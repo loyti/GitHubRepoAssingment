1f4bc6db69ecb133d435a1674ae8b8c7c68521ab
loyti <brice@loyti.com>
2017-06-26 15:14:30 -0700

    updated files

--- WebFundamentals/jQuery/jQuery.js
+++ WebFundamentals/jQuery/jQuery.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
-            $('#hide').click(function(){
-                alert("You have hidden all \"p\" elements!");
-                $('p').hide();
-            })
-            $('#show').click(function(){
-                alert("You have shown all \"p\" elements!");
-                $('p').show();
-            })
-            $('#clickMe').hover(function(){
-                $('.bodyBottom').css('background', 'rgba(0,0,0,.5)')
-            }, function(){$('.bodyBottom').css('background', '#fff')})
-            $('#clickMe').click(function(){
-                $('.bodyTop').toggle("slow");
-            })
-            $('#clickMe1').click(function(){
-                $('.bodyTop').slideUp();
-            })
-            $('#clickMe2').click(function(){
-                $('.bodyTop').slideDown();
-            })
-            $('#clickMe3').click(function(){
-                $('.bodyTop').slideToggle("fast");
-            })
-            
-        });
+    $('#hide').click(function(){
+        alert("You have hidden all \"p\" elements!");
+        $('p').hide();
+    })
+    $('#show').click(function(){
+        alert("You have shown all \"p\" elements!");
+        $('p').show();
+    })
+    $('#clickMe').hover(function(){
+        $('.bodyBottom').css('background', 'rgba(0,0,0,.5)')
+    }, function(){$('.bodyBottom').css('background', '#fff')
+    })
+    $('#clickMe').click(function(){
+        $('.bodyTop').toggle("slow");
+    })
+    $('#clickMe1').click(function(){
+        $('.bodyTop').slideUp();
+    })
+    $('#clickMe2').click(function(){
+        $('.bodyTop').slideDown();
+    })
+    $('#clickMe3').click(function(){
+        $('.bodyTop').slideToggle("fast");
+    })
+    $('#clickMe1').hover(function(){
+        $('.bodyMiddle').fadeOut('slow')
+    }, function(){$('.bodyMiddle').fadeIn('fast')
+    })
+    $('clickMe3').hover(function(){
+        $(h2).addClass("addColor")
+    })
+});
