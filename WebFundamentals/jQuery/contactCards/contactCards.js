2512a6633b9793a5c93f43eb335dcd9682142579
loyti <brice@loyti.com>
2017-06-27 14:56:25 -0700

    updated html css & js files

--- WebFundamentals/jQuery/contactCards/contactCards.js
+++ WebFundamentals/jQuery/contactCards/contactCards.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $('#collectID').submit(function(e){
+        e.preventDefault();
+        var name = $('.nameFL').val();
+        var email = $('.emailI').val();
+        var description = $('.about').val();    
+        $('.contact').append("<div class=\"card\"><h3 class=\"cName\">" + name + "</h3>" + "<h4 id=\"cEmail\><a href=\"mailto:" + email + "\">" + email + "</a></h4>" + "<p id=\"cAbout\" style=\"display:none\">" + description + "</p></div>");
+    })
+    $(document).on('click', '.card', function (){
+        $(this).children().toggle();
+    })
 });
