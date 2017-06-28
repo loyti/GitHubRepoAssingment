f9b2e05be6991b092e04fb36bf450ae782e826a3
loyti <brice@loyti.com>
2017-06-28 16:13:45 -0700

    updated html css & js files

--- WebFundamentals/ajax/pokemon/pokemon.js
+++ WebFundamentals/ajax/pokemon/pokemon.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     for (var i = 1; i < 152; i++){
-        $(".pokemonI").append("<img src=\"http://pokeapi.co/media/img/" + i + ".png\" class=\"pokemonNew\" id=\"" + i + "\">");
+        $(".pokedexI").append("<img src=\"http://pokeapi.co/media/img/" + i + ".png\" class=\"pokemonNew\" id=\"" + i + "\">");
     };
-    $('.pokemonI').on('click', '.pokemonNew', function(e){
+    $('.pokedexI').on('click', '.pokemonNew', function(e){
         var pid = e.currentTarget.id;
         $('.pokeName').html("");
         $('#pokeHeight').html("");
         $('#pokeWeight').html("");
         $('#pokeType').html("");
-        
+        $('.pokeI').html("");
+
         console.log(e);
         console.log("start data request");
         $.get('http://pokeapi.co/api/v2/pokemon/' + pid + "/", function(pokedex){
@@ -18,7 +19,7 @@ $(document).ready(function(){
                 console.log("test")
             }
             $('.pokeName').append('<h1>' + pokedex.name.toUpperCase() + '</h1>');
-            $('.pokeI').append('<img src=\"' + pokedex.sprites.front_default + '\">');
+            $('.pokeI').attr('src', pokedex.sprites.front_default);
             $('#pokeHeight').append('<p>' + pokedex.weight + '</p>');
             $('#pokeWeight').append('<p>' + pokedex.height + '</p>');
             for (var i = 0; i < pokedex.stats["weight"]; i++){
