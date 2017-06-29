4a8018c4f8f047f257b85296bc0a43735f66bed7
loyti <brice@loyti.com>
2017-06-29 11:12:28 -0700

    updated html css & js

--- WebFundamentals/ajax/goThrones/goThrones.js
+++ WebFundamentals/ajax/goThrones/goThrones.js
@@ -2,16 +2,24 @@ $(document).ready(function(){
     for (var i = 6; i < 12; i++){
         $.get('https://anapioficeandfire.com/api/houses/' + i + '/', function(data){
             console.log(data);
-            $('.house').append('<div id="house' + i + ' class="newHouse"><h1 class="houseName">' + data.name + '</h1>' + '<h2 class="houseRegion">' + data.region + '</h2>' + '<p class="houseWords">' + data.words + '</p></div>');
-        })
-    $('.house').on('click', '.house', function(e){
-        hid = e.currentTarget.id;
+            
+            $('.house').append('<div class="newHouse"><h1 class="houseName">' + data.name + '</h1>' + '<h2 class="houseRegion">' + data.region + '</h2>' + '<p class="houseWords">' + data.words + '</p></div>');
+            $('.house').attr('id', i);
+        })};
+    $(document).on('click', '.house', function(e){
+        var houseID = e.currentTarget.id;
         $('.bottom').html("");
-        $('.houseRegion').html("");
-        $('.houseWords').html("");
+        
+        $('.newHouseName').html("");
+        $('.newHouseRegion').html("");
+        $('.newHouseWords').html("");
+        console.log(e);
         console.log("start data request");
-        $.get('https://anapioficeandfire.com/api/houses/' + hid + '/', function(data){
-                $('.bottom').append('<h1 class="houseName">' + data.name + '</h1>' + '<h2 class="houseRegion">' + data.region + '</h2>' + '<p class="houseWords">' + data.words + '</p></div>');
+            $.get(('https://anapioficeandfire.com/api/houses/' + houseID + '/'), function(event){
+                console.log("event", event);
+                $('.bottom').append('<div class="newHouse"><h1 class="newHouseName">' + event.name + '</h1>' + '<h2 class="newHouseRegion">' + event.region + '</h2>' + '<p class="newHouseWords">' + event.words + '</p></div>');
         });
-    });};
+    });
+        
+    
 });
