992df2221672718370d1b597f649d8c0b7d13f66
loyti <brice@loyti.com>
2017-06-29 08:13:24 -0700

    updated html css & js files

--- WebFundamentals/ajax/goThrones/goThrones.js
+++ WebFundamentals/ajax/goThrones/goThrones.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    
+    for (var i = 6; i < 12; i++){
+        $.get('https://anapioficeandfire.com/api/houses/' + i + '/', function(data){
+            console.log(data);
+            $('.house').append('<div id="house' + i + ' class="newHouse"><h1 class="houseName">' + data.name + '</h1>' + '<h2 class="houseRegion">' + data.region + '</h2>' + '<p class="houseWords">' + data.words + '</p></div>');
+        })
+    $('.house').on('click', '.house', function(e){
+        hid = e.currentTarget.id;
+        $('.bottom').html("");
+        $('.houseRegion').html("");
+        $('.houseWords').html("");
+        console.log("start data request");
+        $.get('https://anapioficeandfire.com/api/houses/' + hid + '/', function(data){
+                $('.bottom').append('<h1 class="houseName">' + data.name + '</h1>' + '<h2 class="houseRegion">' + data.region + '</h2>' + '<p class="houseWords">' + data.words + '</p></div>');
+        });
+    });};
 });
