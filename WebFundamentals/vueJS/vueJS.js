fa95af31855be31205426ce9c69e571db7b02b35
loyti <brice@loyti.com>
2017-06-29 13:55:41 -0700

    updated html & js files

--- WebFundamentals/vueJS/vueJS.js
+++ WebFundamentals/vueJS/vueJS.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
     Vue.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-})
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
-  }
-})
+        props: ['todo'],
+        template: '<li>{{ todo.text }}</li>'
+    })
+    var app7 = new Vue({
+        el: '#app-7',
+        data: {
+            groceryList: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' },
+                { id: 3, text: 'Milk' },
+                { id: 4, text: 'Cookies' },
+                { id: 5, text: '... & Many More Vegetables' },
+            ]
+        }
+    })
 })
