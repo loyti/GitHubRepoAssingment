5803295990a7c1d4ffc4dac052676d29e43ce6e1
loyti <brice@loyti.com>
2017-06-29 19:11:17 -0700

    updated html & js files

--- WebFundamentals/vueJS/vueJS.js
+++ WebFundamentals/vueJS/vueJS.js
@@ -1,19 +1,43 @@
-$(document).ready(function(){
-    Vue.component('todo-item', {
-        props: ['todo'],
-        template: '<li>{{ todo.text }}</li>'
-    })
-    var app7 = new Vue({
-        el: '#app-7',
-        data: {
-            groceryList: [
-                { id: 0, text: 'Vegetables' },
-                { id: 1, text: 'Cheese' },
-                { id: 2, text: 'Whatever else humans are supposed to eat' },
-                { id: 3, text: 'Milk' },
-                { id: 4, text: 'Cookies' },
-                { id: 5, text: '... & Many More Vegetables' },
-            ]
+var ToDoMaker = (function (){
+        var id = 0;
+        return function (name, description){
+            id += 1;
+            this.name = name;
+            this.description = description;
+            this.status = false;
+            this.id = id;
+            this.createdAt = new Date();
         }
-    })
+}());
+new Vue({
+    el: '#todoList',
+    data: {
+        itemsList: [],
+        name:"",
+        description:""
+        
+    },
+    methods: {
+        addItem: function () {
+        var newItem = new ToDoMaker(this.name, this.description);
+        this.itemsList.push(newItem);
+        console.log(newItem);
+        },
+        removeItem: function(loseItem) {
+            console.log("test");
+            for (var i = 0; i < this.itemsList.length; i++){
+                if (this.itemsList[i].id = loseItem){
+                    this.itemsList.splice(i,1);
+                }
+            }
+        },
+        changeStatus: function() {
+            for (var i = 0; i < this.itemsList.length; i++){
+                if ( this.item[i].status = false){
+                    this.itemList.status = true;
+                }
+            console.log(status);
+            }
+        }
+    }    
 })
