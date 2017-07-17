a49c8fc3829993a65453606109ab9cd4a5a15328
loyti <brice@loyti.com>
2017-07-17 11:45:37 -0700

    updated django files

--- Python/django/testRun/apps/surveys/views.py
+++ Python/django/testRun/apps/surveys/views.py
@@ -1,43 +1,45 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-	session = request.session
-	session['first_name'] = session.get['first_name']
-	session['last_name'] = session.get['last_name']
-	context = {
-		'first_name' : session.form['first_name'],
-		'last_name' : session.form['last_name'],
-		'location' : session.form['location'],
-		'favLang' : session.form['favLang'],
-	}
-
-	return render(request, 'survey/index.html', context)
+	request.session['sessionCounter'] = request.session.get('sessionCounter', 0)
+	
+	return render(request, 'surveys/index.html')
 
 def postSurvey(request):
-	session = request.session
-	if "session" not in request.session:
-		redirect('/')
-	elif request.method == "POST":
-		form = form(request.POST)
-		session['first_name'] = session.get['first_name']
-        	session['last_name'] = session.get['last_name']
-		session['location'] = session.get['location']
-		sessoin['favLang'] = session.get['favLang']
-		context = {
-                	'first_name' : session.form['first_name'],
-                	'last_name' : session.form['last_name'],
-                	'location' : session.form['location'],
-			'favLang' : session.form['favLang'],
-		}
-		return redirect('/surveyView')
+	errors = []
+	if request.method == "POST":
+		if request.POST['first_name'] == "":
+			errors.append("Please provide your First Name")
+		if request.POST['last_name'] == "":
+			errors.append("Please provide your Last Name")
+		if request.POST['email'] == "":
+			errors.append("Please provide your Email")
+
+		if len(errors) > 0: 
+			for error in errors: 
+				messages.error(request, error)
+			return redirect('/surveys')
+		else: 
+			request.session['first_name'] = request.POST['first_name']
+			request.session['last_name'] = request.POST['last_name']
+                	request.session['email'] = request.POST['email']
+                	request.session['location'] = request.POST['location']
+                	request.session['favLang'] = request.POST['favLang']
+                	request.session['comments'] = request.POST['comments']
+			messages.success(request, "Success")
+			request.session['sessionCounter'] += 1
+			return redirect('/surveys/surveyView')
 
 def surveyView(request):
-	session = request.session
 	context = {
-                'first_name' : session.form['first_name'],
-                'last_name' : session.form['last_name'],
-                'location' : session.form['location'],
-                'favLang' : session.form['favLang'],
+                'first_name' : request.session['first_name'],
+                'last_name' : request.session['last_name'],
+		'email' : request.session['email'],
+                'location' : request.session['location'],
+                'favLang' : request.session['favLang'],
+		'comments' : request.session['comments'],
+		'sessionCounter' : request.session['sessionCounter'],
         }
-	return render(request, 'survey/viewSurvey.html', context)
+	return render(request, 'surveys/viewSurvey.html', context)
--- Python/django/testRun/testRun/urls.py
+++ Python/django/testRun/testRun/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', include('apps.firstRun.urls')),
 	url(r'^firstRun/', include('apps.firstRun.urls')),
 	url(r'^blogs/', include('apps.blogs.urls')),
 	url(r'^surveys/', include('apps.surveys.urls')),
