49890b2ffa227826027ef4b8be161dc55e06e94b
loyti <brice@loyti.com>
2017-08-10 11:44:01 -0700

    updated time

--- MEAN/ang/ustz/src/app/app.component.ts
+++ MEAN/ang/ustz/src/app/app.component.ts
@@ -8,43 +8,18 @@ import { DatePipe } from '@angular/common';
 })
 export class AppComponent {
   title = 'United States Time Zones';
-  todayDate = new Date();
-  todayTime = new Date();
-  today;
-  pacTime = this.todayTime;
-  pac = false;
-  mountTime = this.pacTime ;//+ 3600000;
-  mount = false;
-  centTime = this.mountTime ;//+ 3600000;
-  cent = false;
-  eastTime = this.centTime ;//+ 3600000;
-  east = false;
-  whatPac(){
-    !this.pac;
-    if( this.pac == true){
-      this.today = this.pacTime;
-    };
-    console.log(this.pac,this.today);
-  };
-  whatMount(){
-    !this.mount;
-    if(this.mount == true){
-      this.today = this.mountTime;
-    };
-    console.log(this.mount,this.today);
-  };
-  whatCent(){
-    !this.cent;
-    if(this.cent == true){
-      this.today = this.centTime;
-    };
-    console.log(this.cent,this.today);
-  };
-  whatEast(){
-    !this.east;
-    if(this.east == true){
-      this.today = this.eastTime;
-    };
-    console.log(this.east,this.today);
-  };
+  time = new Date();
+  lastTimeZoneSelected = null;
+
+  getDateByZone(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    }
+    this.lastTimeZoneSelected = timezone;
+  }
 }
