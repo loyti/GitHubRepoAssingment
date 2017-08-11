6de697aac41885cff2bb59c38a25dc195b12749b
loyti <brice@loyti.com>
2017-08-10 17:46:49 -0700

    initial commit

--- MEAN/ang/aQuotes/src/app/app.module.ts
+++ MEAN/ang/aQuotes/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; // <-- Import HttpModule
+
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QlistComponent } from './quotes/qlist/qlist.component';
+import { QnewComponent } from './quotes/qnew/qnew.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QlistComponent,
+    QnewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MEAN/ang/aQuotes/src/app/quotes/qlist/qlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qlist',
+  templateUrl: './qlist.component.html',
+  styleUrls: ['./qlist.component.css']
+})
+export class QlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
