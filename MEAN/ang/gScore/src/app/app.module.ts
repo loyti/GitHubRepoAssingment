bbb62679290a4ce6b9eb773b8f429c97d9c0bf07
loyti <brice@loyti.com>
2017-08-14 11:17:35 -0700

    updated gscore

--- MEAN/ang/gScore/src/app/app.module.ts
+++ MEAN/ang/gScore/src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { ApiService } from './api.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
